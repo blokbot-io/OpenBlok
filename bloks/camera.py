8c467756986faf483dd939fbfd8e4961fbe3976b
Justin Merrell <justin.merrell@adiaon.com>
2022-09-08 13:50:07 -0400

    Update camera.py

--- bloks/camera.py
+++ bloks/camera.py
@@ -18,7 +18,7 @@ import numpy as np
 
 print("INFO | camera module loaded")
 
-FPS = 60  # Frames per second
+FPS = 30  # Frames per second
 
 
 # ------------------------ Continuous Capture Thread ------------------------ #
@@ -45,34 +45,32 @@ def continuous_capture():
     cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 2160)    # Set the height of the frame
     # cap.set(cv2.CAP_PROP_AUTO_EXPOSURE, 3)    # Set to 1 for manual or 3 for auto
     cap.set(cv2.CAP_PROP_FPS, FPS)              # Set frames per second
-    cap.set(cv2.CAP_PROP_BUFFERSIZE, 1)         # Set the buffer size to 1
+    cap.set(cv2.CAP_PROP_BUFFERSIZE, 3)         # Set the buffer size to 1
 
     set_manual_exposure(0, 40)
 
+    last_frame = None                           # Last frame taken
     while True:
+        ret, last_frame = cap.read()                 # Read the frame
+
+        if not ret or last_frame is None:
+            print("WARNING | Can't receive frame (stream end?). Exiting ...")
+            config.requested_frame = None
+
         if config.requested_frame is None:
-            ret, frame = cap.read()             # Capture frame from stream
+            # Rotate the frame if needed
+            if config.rotational_offset is not None:
+                time_now = time.time()
 
-            if not ret or frame is None:
-                print("WARNING | Can't receive frame (stream end?). Exiting ...")
-                config.requested_frame = None
+                rotation_matrix = cv2.getRotationMatrix2D(
+                    (config.rotational_offset[0], config.rotational_offset[1]),
+                    config.rotational_offset[2], 1)
+                frame = cv2.warpAffine(last_frame, rotation_matrix, (last_frame.shape[1], last_frame.shape[0]))
 
-            else:
-                # Rotate the frame if needed
-                if config.rotational_offset is not None:
-                    time_now = time.time()
+                print(f"Time to rotate: {time.time() - time_now}")
 
-                    rotation_matrix = cv2.getRotationMatrix2D(
-                        (config.rotational_offset[0], config.rotational_offset[1]),
-                        config.rotational_offset[2], 1)
-                    frame = cv2.warpAffine(frame, rotation_matrix, (frame.shape[1], frame.shape[0]))
-
-                    print(f"Time to rotate: {time.time() - time_now}")
-
-                config.requested_frame = [np.copy(frame), Decimal(time.time())]
-                continue
-
-        cap.grab()              # Clear the buffer
+            config.requested_frame = [np.copy(last_frame), Decimal(time.time())]
+            continue
 
 
 # ---------------------------- Grab Frame Function --------------------------- #
