07d1c30e9218bd11b5ca00f8f2a2854468090a81
Justin Merrell <justin.merrell@adiaon.com>
2022-11-20 20:49:47 -0500

    Time Changes

--- bloks/serial.py
+++ bloks/serial.py
@@ -13,8 +13,8 @@ arduino = serial.Serial(port='/dev/ttyACM0', baudrate=9600, timeout=.3)
 # except serial.serialutil.SerialException as err:
 #     print(f"Error starting serial connection: {err}")
 
-# config.part_velocity = None  # in/s origional .635 then 0.66
-config.part_velocity = Decimal(0.66)
+# config.part_velocity = None  # in/s original .635 then 0.66
+config.part_velocity = Decimal(1.95)
 
 
 TRAVEL_DISTANCE = 40  # in
