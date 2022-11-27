a55fd6f0014a27213c2d2869759dc2f7bcde5cb9
Justin Merrell <merrelljustin@gmail.com>
2022-11-27 16:34:39 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -17,9 +17,9 @@ pytohn_version=$(python3 -c 'import sys; print(".".join(map(str, sys.version_inf
 if [ "$pytohn_version" != "3.10" ]; then
     sudo apt install software-properties-common -y
     yes '' | sudo add-apt-repository ppa:deadsnakes/ppa
-    sudo apt-get --reinstall install python3.10.8 -y
+    sudo apt-get --reinstall install python3.10 -y
 else
-    echo "Python 3.11 already installed"
+    echo "Python 3.10 already installed"
 fi
 
 # ---------------------------------------------------------------------------- #
@@ -27,13 +27,13 @@ fi
 # ---------------------------------------------------------------------------- #
 
 # ----------------------------- Setup Enviroment ----------------------------- #
-REQUIRED_PKG="python3.10.8 -venv"
+REQUIRED_PKG="python3.10 -venv"
 PKG_OK=$(dpkg-query -W --showformat='${Status}\n' $REQUIRED_PKG|grep "install ok installed")
 if [ "" = "$PKG_OK" ]; then
     echo "No $REQUIRED_PKG. Setting up $REQUIRED_PKG..."
     sudo apt-get install python3.10.8-venv -y
 else
-    echo "python3.10.8-venv already installed, skipping..."
+    echo "python3.10 -venv already installed, skipping..."
 fi
 
 if [ ! -d "/opt/blok/env" ]; then
