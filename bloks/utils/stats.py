2e7f9ba9ac3233eaebf58c8226fe551fe709c288
Justin Merrell <merrelljustin@gmail.com>
2022-11-28 19:05:34 -0500

    Added FPS counter

---
+++ bloks/utils/stats.py
@@ -0,0 +1,24 @@
+''' OpenBlok | bloks | utils | stats.py '''
+
+
+class Stats:
+    ''' Running statistics '''
+
+    def __init__(self):
+        self.frame_time_log = []
+
+    def add_frame_time(self, frame_time):
+        '''
+        Stores time stamps for 1 second of frames
+        '''
+        time_difference = frame_time - self.frame_time_log[0]
+        if time_difference > 1:
+            self.frame_time_log.pop(0)
+
+        self.frame_time_log.append(frame_time)
+
+    def fps(self):
+        '''
+        Calculate frames per second
+        '''
+        return len(self.frame_time_log)
