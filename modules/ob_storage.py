08c1ebef1119b10d6d28766b63d29ebbe2461577
Justin Merrell <merrelljustin@gmail.com>
2023-02-07 13:18:16 -0500

    Update ob_storage.py

--- modules/ob_storage.py
+++ modules/ob_storage.py
@@ -114,14 +114,14 @@ class RedisStorageManager():
         '''
         Gets a frame from the redis queue
         '''
-        frame_uuid = self.redis.blpop(queue_name, timeout=5)
+        frame_uuid = self.redis.blpop(queue_name, timeout=30)
         frame_uuid = frame_uuid.decode("utf-8")
 
         frame_bytes = self.redis.hget(f"{queue_name}:{frame_uuid}", "frame")
         frame_nparray = np.asarray(bytearray(frame_bytes), dtype="uint8")
         frame_decoded = cv2.imdecode(frame_nparray, cv2.IMREAD_COLOR)
 
-        frame_timestamp = self.redis.hget(f"{queue_name}:{frame_uuid}", "timestamp").decode("utf-8")
+        frame_timestamp = self.redis.hget(f"{queue_name}:{frame_uuid}", "timestamp")
 
         frame_object = {
             "frame": frame_decoded,
