7ab81cd53f545e124266c9577ba640fd72764101
Justin Merrell <merrelljustin@gmail.com>
2023-02-07 23:49:41 -0500

    Update ob_predictions.py

--- sub_processes/ob_predictions.py
+++ sub_processes/ob_predictions.py
@@ -26,12 +26,17 @@ def run_models():
         next_ready_frame = redis_db.get_frame("roi")
         preprocessed_frame = next_ready_frame['frame']
 
-        new_metadata = {}
+        source = redis_db.get_frame("rotated", frame_uuid=next_ready_frame['source_frame'])
+        source_metadata = {}
+
+        for key, value in source.items():
+            if key != 'frame':
+                source_metadata[key] = value
 
         side, top = location_model.get_location(preprocessed_frame)
 
-        new_metadata['side'] = str(side)
-        new_metadata['top'] = str(top)
+        source_metadata['side'] = str(side)
+        source_metadata['top'] = str(top)
 
         if 0 not in [side[0], side[1], top[0], top[1]] and top[0] > preprocessed_frame.shape[1]//3:
             top[0] = top[0] - preprocessed_frame.shape[1]//3
@@ -50,8 +55,8 @@ def run_models():
                 (side_crop[0], top_crop[0]), axis=1)
             predictions = e2e_model.get_predictions(view_concatenated)
 
-            new_metadata['predictions'] = str(predictions)
-            new_metadata['side_crop'] = str(side_crop)
-            new_metadata['top_crop'] = str(top_crop)
+            source_metadata['predictions'] = str(predictions)
+            source_metadata['side_crop'] = str(side_crop)
+            source_metadata['top_crop'] = str(top_crop)
 
-        redis_db.add_metadata("rotated", next_ready_frame['source_frame'], new_metadata)
+        redis_db.store_frame("predictions", source['frame'], source_metadata)
