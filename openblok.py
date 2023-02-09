b8ac6960c8f2ebf638bee4d9d46f829c44b3eb18
Justin Merrell <merrelljustin@gmail.com>
2023-02-09 11:28:47 -0500

    Update openblok.py

--- openblok.py
+++ openblok.py
@@ -102,7 +102,7 @@ predict_process.daemon = True
 predict_process.start()
 
 annotate_process = multiprocessing.Process(
-    target=ob_annotate_frame.annotate_frame,
+    target=ob_annotate_frame.annotate,
     args=(config.AruCo_corners, config.AruCo_ids, config.AruCo_center_x,
           config.mirror_offset, config.AruCo_px_per_inch)
 )
