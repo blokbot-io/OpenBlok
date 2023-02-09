73e498fffadd775982ef7057ac9503cf0746223e
Justin Merrell <merrelljustin@gmail.com>
2023-02-08 20:34:04 -0500

    Update ob_annotate_frame.py

--- sub_processes/ob_annotate_frame.py
+++ sub_processes/ob_annotate_frame.py
@@ -19,8 +19,7 @@ def annotate(AruCo_corners, AruCo_ids, AruCo_center_x, mirror_offset, AruCo_px_p
     while True:
         predicted = redis_db.get_frame("predicted")
         predicted_frame = predicted['frame']
-        predicted_side = list(predicted['side'])
-        predicted_top = list(predicted['top'])
+
         predicted_preprocessed_shape = list(predicted['preprocessed_shape'])
 
         # Marker Layer
@@ -36,7 +35,41 @@ def annotate(AruCo_corners, AruCo_ids, AruCo_center_x, mirror_offset, AruCo_px_p
         bound_corners = bounding_boxes()
         predicted_frame = annotate.bounding_areas(predicted_frame, bound_corners)
 
-        side = predicted_side
-        top = predicted_top
+        side = list(predicted['side'])
+        top = list(predicted['top'])
         if 0 not in [side[0], side[1], top[0], top[1]] and top[0] > predicted_preprocessed_shape[1]//3:
             top[0] = top[0] - predicted_preprocessed_shape[1]//3
+
+            # ----------------------------- Object Locations ----------------------------- #
+            # Side View
+            predicted_frame = annotate.mark_object_center(
+                predicted_frame,
+                (side[0]+bound_corners[2][0], side[1]+bound_corners[0][1]),
+                (255, 0, 0)
+            )
+
+            side_crop = list(predicted['side_crop'])
+            top_crop = list(predicted['top_crop'])
+
+            predicted_frame = annotate.visualize_crop(
+                predicted_frame,
+                (side_crop[1][0]+bound_corners[2][0],
+                 side_crop[1][1]+bound_corners[2][1]),
+                (side_crop[2][0]+bound_corners[2][0],
+                 side_crop[2][1]+bound_corners[2][1]),
+                (255, 0, 0)
+            )
+
+            # Top View
+            predicted_frame = annotate.mark_object_center(
+                predicted_frame,
+                (top[0]+bound_corners[0][0], top[1]+bound_corners[0][1])
+            )
+
+            predicted_frame = annotate.visualize_crop(
+                predicted_frame,
+                (top_crop[1][0]+bound_corners[0][0],
+                 top_crop[1][1]+bound_corners[0][1]),
+                (top_crop[2][0]+bound_corners[0][0],
+                 top_crop[2][1]+bound_corners[0][1])
+            )
