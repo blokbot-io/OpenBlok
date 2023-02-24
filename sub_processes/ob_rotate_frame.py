76c4b2050aecf561c337e0c5c0a5b8b2e6327095
Justin Merrell <merrelljustin@gmail.com>
2023-02-24 14:49:43 -0500

    Update ob_rotate_frame.py

--- sub_processes/ob_rotate_frame.py
+++ sub_processes/ob_rotate_frame.py
@@ -17,28 +17,32 @@ def rotation_correction(rotation_info):
     redis_db = ob_storage.RedisStorageManager()
 
     while True:
-        frame_object = redis_db.get_frame("raw", delete_frame=False)    # Get frame from queue
+        try:
+            frame_object = redis_db.get_frame("raw", delete_frame=False)    # Get frame from queue
 
-        rotation_matrix = cv2.getRotationMatrix2D(
-            (rotation_info['aruco_center_x'], rotation_info['aruco_center_y']),
-            rotation_info['angle_offset'], 1)
+            rotation_matrix = cv2.getRotationMatrix2D(
+                (rotation_info['aruco_center_x'], rotation_info['aruco_center_y']),
+                rotation_info['angle_offset'], 1)
 
-        last_frame = cv2.warpAffine(
-            frame_object['frame'], rotation_matrix,
-            (frame_object['frame'].shape[1], frame_object['frame'].shape[0])
-        )
+            last_frame = cv2.warpAffine(
+                frame_object['frame'], rotation_matrix,
+                (frame_object['frame'].shape[1], frame_object['frame'].shape[0])
+            )
 
-        metadata = {
-            'timestamp': frame_object['metadata']['timestamp'],
-            'rotationCorrection': {
-                "arucoCenterX": rotation_info['aruco_center_x'],
-                "arucoCenterY": rotation_info['aruco_center_y'],
-                "angleOffset": rotation_info['angle_offset']
+            metadata = {
+                'timestamp': frame_object['metadata']['timestamp'],
+                'rotationCorrection': {
+                    "arucoCenterX": rotation_info['aruco_center_x'],
+                    "arucoCenterY": rotation_info['aruco_center_y'],
+                    "angleOffset": rotation_info['angle_offset']
+                }
             }
-        }
 
-        # Save the frame to Redis
-        redis_db.add_frame("rotated", last_frame, metadata)
+            # Save the frame to Redis
+            redis_db.add_frame("rotated", last_frame, metadata)
 
-        # Sleep to maintain FPS
-        time.sleep(1/30)
+            # Sleep to maintain FPS
+            time.sleep(1/30)
+
+        except Exception as e:
+            print(e)
