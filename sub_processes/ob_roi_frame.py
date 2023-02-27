c566595f9d2e1af345f017b1d1fc76f59e32ed0d
Justin Merrell <merrelljustin@gmail.com>
2023-02-27 10:29:41 -0500

    Update ob_roi_frame.py

--- sub_processes/ob_roi_frame.py
+++ sub_processes/ob_roi_frame.py
@@ -78,7 +78,7 @@ def capture_regions():
         frame = frame_object['frame']
         metadata = frame_object['metadata']
 
-        if time.time() - metadata["timestamp"] > .2:
+        if time.time() - metadata["timestamp"] > .15:
             redis_db.delete_frame("raw", metadata["rawUUID"])
             continue
 
