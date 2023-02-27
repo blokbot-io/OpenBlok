82e99662c4a48190de1db7b0589c34a765f1e710
Justin Merrell <merrelljustin@gmail.com>
2023-02-26 22:25:10 -0500

    Update camera.py

--- bloks/camera.py
+++ bloks/camera.py
@@ -78,7 +78,7 @@ def continuous_capture():
     cap = cv2.VideoCapture(0)                   # Opens the USB camera stream
     cap.set(cv2.CAP_PROP_FRAME_WIDTH, 3840)     # Set the width of the frame
     cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 2160)    # Set the height of the frame
-    cap.set(cv2.CAP_PROP_BUFFERSIZE, 1)         # Set the buffer size to 1
+    cap.set(cv2.CAP_PROP_BUFFERSIZE, 3)         # Set the buffer size to 1
     cap.set(cv2.CAP_PROP_FPS, FPS)              # Set frames per second
     cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'))
 
